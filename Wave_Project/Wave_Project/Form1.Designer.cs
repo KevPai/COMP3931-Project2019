052d63342ad005fa36297b100dac4adc460f35fa
Kevin Pai <KPai555@Hotmail.com>
2019-09-20 16:16:18 -0700

    Draws a simple Sine wave on button click

--- Wave_Project/Wave_Project/Form1.Designer.cs
+++ Wave_Project/Wave_Project/Form1.Designer.cs
@@ -30,22 +30,10 @@ namespace Wave_Project
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(246, 95);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(645, 259);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -66,27 +54,35 @@ namespace Wave_Project
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Enter equation";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(974, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "New Window";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1105, 643);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Wave_Project/Wave_Project/Form1.cs
+++ Wave_Project/Wave_Project/Form1.cs
@@ -19,7 +19,34 @@ namespace Wave_Project
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            Graphics g = this.CreateGraphics();
 
+            Pen pen = new Pen(Brushes.Black, 7.0F);
+
+            float x1 = 0;
+            float y1 = 0;
+
+            float y2 = 0;
+
+            float yEx = 100;
+            float eF = 40;
+
+            for (float x = 0; x < 20; x += 0.1F)
+            {
+                y2 = (float)Math.Sin(x);
+
+                g.DrawLine(pen, x1 * eF, y1 * eF + yEx, x * eF, y2 * eF + yEx);
+
+                x1 = x;
+                y1 = y2;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
