4665ce42b4eae8db13328ef83817754d286965dd
Haojun <hliao20@my.bcit.ca>
2019-12-04 13:31:34 -0800

    edited wav.cs

--- Wav.cs
+++ Wav.cs
@@ -3,6 +3,19 @@ using System.Runtime.InteropServices;
 using static System.Text.Encoding;
 using System.IO;
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
 namespace WaveFile
 {
     public class Wav
@@ -178,5 +191,144 @@ namespace WaveFile
 
     }
 
+    public class WaveHeader
+    {
+        public string sGroupID; // RIFF
+        public uint dwFileLength; // total file length minus 8, which is taken up by RIFF
+        public string sRiffType; // always WAVE
 
-}
+        /// <summary>
+        /// Initializes a WaveHeader object with the default values.
+        /// </summary>
+        public WaveHeader()
+        {
+            dwFileLength = 0;
+            sGroupID = "RIFF";
+            sRiffType = "WAVE";
+        }
+    }
+
+    public class WaveFormatChunk
+    {
+        public string sChunkID;         // Four bytes: "fmt "
+        public uint dwChunkSize;        // Length of header in bytes
+        public ushort wFormatTag;       // 1 (MS PCM)
+        public ushort wChannels;        // Number of channels
+        public uint dwSamplesPerSec;    // Frequency of the audio in Hz... 44100
+        public uint dwAvgBytesPerSec;   // for estimating RAM allocation
+        public ushort wBlockAlign;      // sample frame size, in bytes
+        public ushort wBitsPerSample;    // bits per sample
+
+        /// <summary>
+        /// Initializes a format chunk with the following properties:
+        /// Sample rate: 44100 Hz
+        /// Channels: Stereo
+        /// Bit depth: 16-bit
+        /// </summary>
+        public WaveFormatChunk()
+        {
+            sChunkID = "fmt ";
+            dwChunkSize = 16;
+            wFormatTag = 1;
+            wChannels = 1;
+            dwSamplesPerSec = 11025;
+            wBitsPerSample = 16;
+            wBlockAlign = (ushort)(wChannels * (wBitsPerSample / 8));
+            dwAvgBytesPerSec = dwSamplesPerSec * wBlockAlign;
+        }
+    }
+    public class WaveDataChunk
+    {
+        public string sChunkID;     // "data"
+        public uint dwChunkSize;    // Length of header in bytes
+        public short[] byteArray;  // 8-bit audio
+
+        /// <summary>
+        /// Initializes a new data chunk with default values.
+        /// </summary>
+        public WaveDataChunk()
+        {
+            byteArray = new short[0];
+            dwChunkSize = 0;
+            sChunkID = "data";
+        }
+    }
+    public class WaveGenerator
+    {
+        // Header, Format, Data chunks
+        WaveHeader header;
+        WaveFormatChunk format;
+        WaveDataChunk data;
+
+        /// <snip>
+        public WaveGenerator(double[] samples)
+        {
+            // Init chunks
+            header = new WaveHeader();
+            format = new WaveFormatChunk();
+            data = new WaveDataChunk();
+
+            // Fill the data array with sample data
+
+            
+            // Number of samples = sample rate * channels * bytes per sample
+            uint numSamples = format.dwSamplesPerSec * format.wChannels;
+
+            // Initialize the 16-bit array
+            Debug.WriteLine(samples[0]);
+            data.byteArray = Array.ConvertAll(samples, x => ((short)x));
+            Debug.WriteLine(data.byteArray[0]);
+            Debug.WriteLine(data.byteArray.Length);
+            //data.shortArray = Array.ConvertAll(samples, x => ((short)x));
+
+
+            // Calculate data chunk size in bytes
+            data.dwChunkSize = (uint)(data.byteArray.Length * (format.wBitsPerSample / 8));
+            
+
+
+        }
+        public void Save(string filePath)
+        {
+
+            FileStream fileStream = new FileStream(filePath, FileMode.Create);
+
+            // Use BinaryWriter to write the bytes to the file
+            BinaryWriter writer = new BinaryWriter(fileStream);
+
+            // Write the header
+            writer.Write(header.sGroupID.ToCharArray());
+            writer.Write(header.dwFileLength);
+            writer.Write(header.sRiffType.ToCharArray());
+
+            // Write the format chunk
+            writer.Write(format.sChunkID.ToCharArray());
+            writer.Write(format.dwChunkSize);
+            writer.Write(format.wFormatTag);
+            writer.Write(format.wChannels);
+            writer.Write(format.dwSamplesPerSec);
+            writer.Write(format.dwAvgBytesPerSec);
+            writer.Write(format.wBlockAlign);
+            writer.Write(format.wBitsPerSample);
+
+            // Write the data chunk
+            writer.Write(data.sChunkID.ToCharArray());
+            writer.Write(data.dwChunkSize);
+            foreach (short dataPoint in data.byteArray)
+            {
+                writer.Write(dataPoint);
+            }
+
+            writer.Seek(4, SeekOrigin.Begin);
+            uint filesize = (uint)writer.BaseStream.Length;
+            writer.Write(filesize - 8);
+
+            // Clean up
+            writer.Close();
+            fileStream.Close();
+        }
+    }
+    
+
+    
+ }
